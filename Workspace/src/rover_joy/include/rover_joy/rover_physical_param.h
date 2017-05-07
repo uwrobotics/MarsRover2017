feec18411fcd6cc5d1870fe97ebc0cf223314c49
Chunshang Li <jack.chunshang.li@gmail.com>
2017-05-07 01:31:08 -0400

    wrote odom code

--- Workspace/src/rover_joy/include/rover_joy/rover_physical_param.h
+++ Workspace/src/rover_joy/include/rover_joy/rover_physical_param.h
@@ -2,3 +2,4 @@ const float MAX_X = 2.0;   // m/s
 const float MAX_Z = 2.0; // rad/s
 const float RADIUS = 0.15; // 30cm wheels
 const float WHEEL_DISTANCE = 0.865; //m
+const float GEAR_RATIO = 53.5714286;
--- Workspace/src/rover_joy/src/rover_encoder_odom.cpp
+++ Workspace/src/rover_joy/src/rover_encoder_odom.cpp
@@ -1,17 +1,18 @@
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
+#include "tf/transform_datatypes.h"
 #include "roboteq_msgs/Feedback.h"
-
+#include "rover_joy/rover_physical_param.h"
 
 double enc_vel_left = 0;
 double enc_vel_right = 0;
 
-void left_feeback_callback(roboteq_msgs::Feedback &msg) {
-
+void left_feeback_callback(const roboteq_msgs::Feedback &msg) {
+    enc_vel_left = msg.measured_velocity; // rad/s
 }
 
-void right_feedback_callback(roboteq_msgs::Feedback &msg) {
-
+void right_feedback_callback(const roboteq_msgs::Feedback &msg) {
+    enc_vel_right = msg.measured_velocity; // rad/s
 }
 
 
@@ -20,15 +21,21 @@ int main(int argc, char** argv){
 
   ros::NodeHandle n;
 
-  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 1);
+  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("husky_velocity_controller/odom", 1);
   ros::Subscriber left_feeback_sub = n.subscribe("/left/feedback", 1, left_feeback_callback);
   ros::Subscriber right_feeback_sub = n.subscribe("/right/feedback", 1, right_feedback_callback);
 
-  double pos_x = 0.0;
-  double pos_y = 0.0;
-  double pos_th = 0.0;
+  // integrated positions
+  double pose_x = 0.0;
+  double pose_y = 0.0;
+  double pose_th = 0.0;
 
-  double vx, vy, vth;
+  // wheel velocities
+  double v_left, v_right;
+
+  // velocities
+  double v_x, v_th; // pose_y velocity is always zero
+  const double v_y = 0;
 
   ros::Time current_time, last_time;
   current_time = ros::Time::now();
@@ -42,19 +49,25 @@ int main(int argc, char** argv){
 
     current_time = ros::Time::now();
 
-    vx = 0;
-    vy = 0;
-    vth = 0;
+    v_left = enc_vel_left / GEAR_RATIO * RADIUS;
+    v_right = enc_vel_right / GEAR_RATIO * RADIUS;
+
+    v_x = (v_right + v_left) / 2;
+    v_th = (v_right - v_left) / WHEEL_DISTANCE;
 
     //compute odometry in a typical way given the velocities of the robot
     double dt = (current_time - last_time).toSec();
-    double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
-    double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
-    double delta_th = vth * dt;
+    double delta_x = (v_x * cos(pose_th) - v_y * sin(pose_th)) * dt;
+    double delta_y = (v_x * sin(pose_th) + v_y * cos(pose_th)) * dt;
+    double delta_th = v_th * dt;
 
-    x += delta_x;
-    y += delta_y;
-    th += delta_th;
+    pose_x += delta_x;
+    pose_y += delta_y;
+    pose_th += delta_th;
+
+    ROS_INFO("VEL: x=%f y=%f th=%f POS: x=%f y=%f th=%f", v_x, v_y, v_th*180/M_PI, pose_x, pose_y, pose_th*180/M_PI);
+
+    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(pose_th);
 
     //next, we'll publish the odometry message over ROS
     nav_msgs::Odometry odom;
@@ -62,16 +75,16 @@ int main(int argc, char** argv){
     odom.header.frame_id = "odom";
 
     //set the position
-    odom.pose.pose.position.x = x;
-    odom.pose.pose.position.y = y;
+    odom.pose.pose.position.x = pose_x;
+    odom.pose.pose.position.y = pose_y;
     odom.pose.pose.position.z = 0.0;
     odom.pose.pose.orientation = odom_quat;
 
     //set the velocity
     odom.child_frame_id = "base_link";
-    odom.twist.twist.linear.x = vx;
-    odom.twist.twist.linear.y = vy;
-    odom.twist.twist.angular.z = vth;
+    odom.twist.twist.linear.x = v_x;
+    odom.twist.twist.linear.y = v_y;
+    odom.twist.twist.angular.z = v_th;
 
     //publish the message
     odom_pub.publish(odom);
