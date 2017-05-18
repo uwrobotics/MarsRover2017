bc07007f7a216270ea8b0af356ae09b52abdd1f9
Chunshang Li <jack.chunshang.li@gmail.com>
2017-05-17 21:56:22 -0400

    changed rover setting to 24V

--- Workspace/src/rover_joy/include/rover_joy/rover_physical_param.h
+++ Workspace/src/rover_joy/include/rover_joy/rover_physical_param.h
@@ -3,5 +3,5 @@ const float MAX_Z = 2.0; // rad/s
 const float RADIUS = 0.15; // 30cm wheels
 const float WHEEL_DISTANCE = 0.865; //m
 const float GEAR_RATIO = 53.5714286;
-const float MAX_RPM = 2500;
+const float MAX_RPM = 5600;
 const float RPM_PER_RAD = 9.549296585513702;
