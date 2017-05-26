97c3de873e23f7111ecd70ed95f3b66a5a61eb8a
Ivy Xing <r5xing@uwaterloo.ca>
2017-05-26 18:14:56 -0400

    Added science services

---
+++ Workspace/src/science_services/src/collect_sensor_start_server.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+##############################################################
+# 
+# @file      collect_sensor_start_server.py
+# @author    Ivy Xing
+# 
+# Description:
+# Service that sends a start signal to CAN to start collecting
+# sensor info.
+# 
+# Request:
+# int32  doesn't do anything
+# 
+# Response:
+# 1      doesn't do anything
+# 
+# How to run this service:
+# - rosrun science_services collect_sensor_start_server.py
+# 
+# - rosservice call /start_collect_sensor 1
+#
+##############################################################
+
+
+from science_services.srv import collect_sensor_start_service
+import rospy
+import struct
+from can_msgs.msg import Frame
+
+# service callback
+def start_collect_sensor_handle(req):
+    frame = Frame()
+    frame.id = 711
+    frame.is_rtr = False
+    frame.is_extended = False
+    frame.is_error = False
+    frame.dlc = 8
+    frame.data = str(bytearray(struct.pack('i', 1)))
+    pub.publish(frame)
+    print "Start sent"
+    return 1
+
+def collect_sensor_start_server():
+    global pub
+    pub = rospy.Publisher('/CAN_transmitter', Frame)
+    rospy.init_node('collect_sensor_start_server') #name of the service to be run, doesn't really matter
+    s = rospy.Service('start_collect_sensor', collect_sensor_start_service, start_collect_sensor_handle)
+#"start_collect_sensor" is the thing that you call in a rosservice call (e.g. rosservice call /start_collect_sensor 1)
+#collect_sensor_start_service is the name of the .srv file in the srv folder
+#start_collect_sensor_handle is the name of the function to call when the service is called
+    print "Ready to run start_collect_sensor script."
+    rospy.spin()
+
+if __name__ == "__main__":
+    collect_sensor_start_server()
+
