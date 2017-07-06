8fe1936f5c3a543f9461c61e723a6c4800204afb
UWMarsRoverTeam <uwmarsroverteam@gmail.com>
2017-07-06 04:20:08 +0000

    Update docker-build.sh

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -2,13 +2,13 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cp -r ${DIR}/scripts ${DIR}/${1}/scripts
+cp -r ${DIR}/scripts ${DIR}/docker/${1}/scripts
 
 docker build -t uwrt/ubuntu:${1} ${DIR}/docker/${1}
 
 RES=$?
 
-rm -r ${DIR}/${1}/scripts
+rm -r ${DIR}/docker/${1}/scripts
 
 if [ ${RES} -ne 0 ]; then
     echo "Error: ${1} docker build failed"
